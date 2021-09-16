4ae1091338b482753b188e425d7eb055f675d8ba
Lucas Schmidt <lucascostaschmidt@gmail.com>
2021-09-15 23:00:04 -0300

    Alteração dia 15/09

--- Js/CalcControler.js
+++ Js/CalcControler.js
@@ -34,26 +34,65 @@ class CalcControler
         this._operation = [];
     }
 
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
     getLastOperation(){
-        return this._operation[this._operation.length - 1];
+        return this._operation[this._operation.length-1];
+        
     }
 
     setLastOperation(value){
-        this._operation[this._operation.length - 1] = value;
+        this._operation[this._operation.length-1] = value;
     }
 
     isOperator(value){
         
         return (["+", "-", "*", "/"].indexOf(value) > -1);
-       
+    }
+
+    pushOperation(value){
+        this._operation.push(value);
+        if (this._operation.length > 3){
+            
+            this.calc();
+        }
+    }
+
+    calc(){
+
+        let last = this._operation.pop();
+        
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+        this.setLastNumberToDisplay();
+
+    }
+
+    setLastNumberToDisplay(){
+        let lastNumber;
+
+        for (let i = this._operation.length-1; i >= 0; i--) {
+            
+            if (!this.isOperator(this._operation[i]))
+            {
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
     }
 
     addOperation(value){
-        console.log('A', isNaN(this.getLastOperation()))
+        console.log('A', value, isNaN(this.getLastOperation()))
+        console.log(this.isOperator(value));
 
         if(isNaN(this.getLastOperation())){
-
- 
             //string
             if(this.isOperator(value)){
                 //change operation
@@ -61,17 +100,25 @@ class CalcControler
 
             }else if(isNaN(value)) {
 
-                console.log(value);
+                console.log("outra coisa", value);
 
             } else {
             
-                this._operation.push(value);
+                this.pushOperation(value);
+                this.setLastNumberToDisplay();
 
             }
         } else {
-            //Number
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+                if(this.isOperator(value)){
+
+                    this.pushOperation(value);
+
+                }else{
+
+                    let newValue = this.getLastOperation().toString() + value.toString();
+                    this.setLastOperation(parseInt(newValue));
+                    this.setLastNumberToDisplay();
+                }
         }
 
         console.log(this._operation);
@@ -87,22 +134,22 @@ class CalcControler
             case 'c':
                 this.clearAll();
                 break;
-            case 'divisao':
+            case '/':
                 this.addOperation('/');
             break;
-            case 'multiplicacao':
+            case '*':
                 this.addOperation('*');
             break;
-            case 'soma':
+            case '+':
                 this.addOperation('+');
             break;
-            case 'subtracao':
+            case '-':
                 this.addOperation('-');
             break;
-            case 'ponto':
+            case '.':
                 this.addOperation('.');
             break;
-            case 'igual':
+            case '=':
                 
             break;
 
